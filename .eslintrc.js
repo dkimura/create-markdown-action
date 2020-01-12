088db573d24427a6d8de95591516564dcf18a84b
Daichi Kimura <dkimura@bdd.xyz>
2020-01-12 13:08:34 +0900

    feat: Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ['globis/base'],
+  parserOptions: {
+    project: './tsconfig.json',
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import fs from 'fs'
+import path from 'path'
+
+import core from '@actions/core'
+
+const generateFile = (outputPath: string, data: string) =>
+  fs.writeFileSync(outputPath, data)
+
+const generateData = (title: string, body: string) => `
+---
+name: ${title}
+---
+
+${body}
+`
+
+try {
+  const fileName = core.getInput('fileName')
+  const title = core.getInput('title')
+  const body = core.getInput('body')
+
+  const data = generateData(title, body)
+  const outputPath = path.resolve('.', fileName)
+
+  generateFile(outputPath, data)
+} catch (error) {
+  core.setFailed(error.message)
+}
